d88aabb7a75489d2babe98f51a186259da3dd8dd
Ziqi Wang <ziqi.wang39@gmail.com>
2021-09-17 15:03:10 +1000

    Import Mongoose

--- src/controllers/course.js
+++ src/controllers/course.js
@@ -1,7 +1,20 @@
-function addCourse(req, res) {}
+const Course = require("../models/course");
 
-function getAllCourse(req, res) {
-  res.send([]);
+async function addCourse(req, res) {
+  const { name, code, description } = req.body;
+  //need to valid data
+  const course = new Course({
+    name,
+    code,
+    description,
+  });
+  await course.save();
+  return res.json(course);
+}
+
+async function getAllCourse(req, res) {
+  const courses = await Course.find();
+  res.json(courses);
 }
 function getCourseById(req, res) {}
 function updateCourseById(req, res) {}
--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@ const express = require("express");
 const cors = require("cors");
 const helmet = require("helmet");
 const routes = require("./routes/index");
+const connectToDB = require("./utils/db");
 
 PORT = process.env.PORT || 3000;
 
@@ -13,6 +14,8 @@ app.use(express.json());
 app.use(helmet());
 
 app.use("/api", routes);
+
+connectToDB(); //因为我们已经对connection的几种情况进行了逻辑处理所以这里不需要.then（）处理。
 
 app.listen(PORT, () => {
   console.log(`Servering is running on port ${PORT}...`);
