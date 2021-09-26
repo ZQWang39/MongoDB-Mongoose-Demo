e94601718b03120446057eceb8fdb6342ccaeb69
Ziqi Wang <ziqi.wang39@gmail.com>
2021-09-26 16:56:33 +1000

    Add user registeration to route

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,10 +1,12 @@
 const express = require("express");
 const courseRouter = require("./course");
 const studentRouter = require("./student");
+const userRouter = require("./user");
 
 const router = express.Router();
 
 router.use("/courses", courseRouter);
 router.use("/students", studentRouter);
+router.use("/user", userRouter);
 
 module.exports = router;
