14d8cf977fe72a78a861de1a11f6f2c10007e79d
Ziqi Wang <ziqi.wang39@gmail.com>
2021-09-26 18:32:19 +1000

    Add token generator to auth

--- src/routes/index.js
+++ src/routes/index.js
@@ -2,11 +2,13 @@ const express = require("express");
 const courseRouter = require("./course");
 const studentRouter = require("./student");
 const userRouter = require("./user");
+const authRouter = require("./auth");
 
 const router = express.Router();
 
 router.use("/courses", courseRouter);
 router.use("/students", studentRouter);
 router.use("/user", userRouter);
+router.use("/auth", authRouter);
 
 module.exports = router;
