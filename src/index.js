3d9aad4709ab0ed2c44018ff53a009d801d3e721
Ziqi Wang <ziqi.wang39@gmail.com>
2021-09-27 15:17:01 +1000

    Test middleware

--- src/index.js
+++ src/index.js
@@ -1,22 +1,8 @@
-require("dotenv").config();
-const express = require("express");
-require("express-async-errors");
-const cors = require("cors");
-const helmet = require("helmet");
-const routes = require("./routes/index");
+const app = require("./app");
+
 const connectToDB = require("./utils/db");
-const errorHandler = require("./middleware/errorHandler");
 
 PORT = process.env.PORT || 3000;
-
-const app = express();
-app.use(cors());
-app.use(helmet());
-app.use(express.json());
-app.use(helmet());
-
-app.use("/api", routes);
-app.use(errorHandler);
 
 connectToDB(); //因为我们已经对connection的几种情况进行了逻辑处理所以这里不需要.then（）处理。
 
